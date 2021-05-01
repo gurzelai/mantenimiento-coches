97c5538f8e62c5a053813c4bed814822a33e8171
gurzelai <gorkaurzelai2000@gmail.com>
2021-05-01 17:27:03 +0200

    layouts nuevo coche, mantenimiento, reparacion

---
+++ app/src/main/java/com/gurzelai/mantenimientocoches/NuevaReparacion.java
@@ -0,0 +1,14 @@
+package com.gurzelai.mantenimientocoches;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NuevaReparacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nueva_reparacion);
+    }
+}
---
+++ app/src/main/java/com/gurzelai/mantenimientocoches/NuevoCoche.java
@@ -0,0 +1,14 @@
+package com.gurzelai.mantenimientocoches;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NuevoCoche extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo_coche);
+    }
+}
