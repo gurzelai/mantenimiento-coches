1fdee0cfd4d29a4bfab28b68f697484550b68ef0
gurzelai <gorkaurzelai2000@gmail.com>
2021-05-02 17:46:25 +0200

    date picker añadido

--- app/src/main/java/com/gurzelai/mantenimientocoches/layout/NuevoCambio.java
+++ app/src/main/java/com/gurzelai/mantenimientocoches/layout/NuevoCambio.java
@@ -1,15 +1,18 @@
 package com.gurzelai.mantenimientocoches.layout;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.gurzelai.mantenimientocoches.Cambio;
+import com.gurzelai.mantenimientocoches.DatePickerFragment;
 import com.gurzelai.mantenimientocoches.R;
 
 public class NuevoCambio extends AppCompatActivity {
@@ -28,9 +31,11 @@ public class NuevoCambio extends AppCompatActivity {
     private void reconocer() {
         btnConfirmar = findViewById(R.id.btnConfirmar);
         etDescripcion = findViewById(R.id.etDescripcion);
+        etExplicacion = findViewById(R.id.etExplicacion);
         etTaller = findViewById(R.id.etTaller);
         etCoste = findViewById(R.id.etCoste);
         etFecha = findViewById(R.id.etFecha);
+        etFecha.setOnClickListener(v-> showDatePickerDialog());
     }
 
     public Cambio crearNuevoCambio() {
@@ -57,5 +62,17 @@ public class NuevoCambio extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    private void showDatePickerDialog() {
+        DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                // +1 because January is zero
+                final String selectedDate = day + " / " + (month+1) + " / " + year;
+                etFecha.setText(selectedDate);
+            }
+        });
+
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
